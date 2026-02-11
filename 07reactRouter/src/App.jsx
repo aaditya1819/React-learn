4d803a42a015062321dd65d4d7c36730c23548a8
Aaditya Dhanwate <aadityadhanwate830@gmail.com>
2026-02-11 15:11:06 +0530

    Fix React Router setup and add User/Github routes

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />} 
+      />
+    </Route>
+  )
+)
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1 className='bg-green-600 p-4 '>  React Router</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
